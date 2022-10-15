2433138f77e11503436e45fade2a3e6a2de6bbd9
Sa1ntway <fryyystandoff@gmail.com>
2022-10-15 11:20:16 +0300

    if else

--- lesson4/task4/script.js
+++ lesson4/task4/script.js
@@ -22,7 +22,38 @@
 
 
 
-let age = prompt('Введите ваш возраст')
-if (age < 18) {
-    alert('true')
+// let age = Number(prompt('Введите ваш возраст'));
+// if (age < 18 || age > 25) {
+//     alert(true);
+// }
+// else {
+//     alert(false);
+// }
+
+
+
+
+
+// let age = Number(prompt('Введите ваш возраст'));
+// let statement = age < 18 || age > 25;
+// if (!statement) {
+//     alert(false);
+// }
+// else {
+//     alert(true);
+// }
+
+
+const hour = Number(prompt('Введите время без ноликов'))
+if (hour < 8 || hour > 23) {
+    alert('Не работает')
 }
+
+else {
+    alert('Работает')
+}
+
+
+
+
+
