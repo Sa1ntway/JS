cfd912baa393c431dffd1f4e28f892609dce3415
Sa1ntway <fryyystandoff@gmail.com>
2023-03-04 13:28:42 +0300

    04.02.23 functions easy level completed

--- JS FUNCTIONS/lesson2/task1/script.js
+++ JS FUNCTIONS/lesson2/task1/script.js
@@ -62,3 +62,181 @@
 //     return flag
 // }
 // console.log(check([1, 2, 3, 4, 5]))
+
+
+
+
+// PRESENTATION PRACTICUM EASY
+// task 2
+// function check() {
+//     let a = +prompt('Введите целое число');
+//     if (a > 0) {
+//         return true;
+//     }
+//     return false;
+
+// }
+// console.log(check())
+
+
+
+
+
+// // task3
+// function check(word) {
+//     let reversedWord = word.split('').reverse().join('');
+//     if (reversedWord == word) {
+//         return true;
+//     }
+//     return false;
+
+
+// }
+// console.log(check('привет'))
+
+
+
+
+
+// task4
+// function count(num) {
+//     if (num > 0) {
+//         let changeNum = num * -1;
+//         for (let i = changeNum; i <= num; i += 2) {
+//             console.log(i)
+//         }
+
+//     }
+//     else {
+//         let changeNum = num * -1;
+//         for (let i = changeNum; num <= i; i -= 2) {
+//             console.log(i)
+//         }
+//     }
+
+// }
+
+// count(-5)
+
+
+
+
+
+// task 5
+// function field(circle, rectangle, right_triangle) {
+//     let a = +prompt('Введите a');
+//     let b = +prompt('Введите b');
+//     let R = +prompt('Введите R');
+//     let right_triangleFormula = a * b / 2;
+//     let rectangleFormula = a * b;
+//     let circleFormula = 3.14 * R * R;
+//     rectangle = rectangleFormula;
+//     circle = circleFormula;
+//     right_triangle = right_triangleFormula;
+//     console.log('Площадь прямоугольника: ' + rectangle, 'Площадь круга: ' + circle, 'Площадь прямоугольного треугольника: ' + right_triangle);
+
+// }
+// field()
+
+
+
+
+
+// task 6
+// function calc(num1, num2, operation) {
+//     let add = num1 + num2;
+//     let substract = num1 - num2;
+//     let division = num1 / num2;
+//     let mult = num1 * num2;
+//     if (operation == '+') {
+//         return add;
+//     }
+//     else if (operation == '-') {
+//         return substract;
+//     }
+//     else if (operation == '/') {
+//         return division;
+//     }
+//     else if (operation == '*') {
+//         return mult;
+//     }
+
+// }
+// console.log(calc(2, 5, '*'));
+
+
+
+
+
+
+
+
+// task 6 my
+// function calc(num1, num2) {
+//     let add = num1 + num2;
+//     let substract = num1 - num2;
+//     let division = num1 / num2;
+//     let mult = num1 * num2;
+//     let ask = +prompt(`Введите какую операцию вы бы хотели сделать
+//     1. add
+//     2. substract
+//     3. division
+//     4. mult`)
+
+//     if (ask == 1) {
+//         console.log(add);
+//     }
+//     else if (ask == 2) {
+//         console.log(substract);
+//     }
+//     else if (ask == 3) {
+//         console.log(division);
+//     }
+//     else if (ask == 4) {
+//         console.log(mult)
+//     }
+
+// }
+// calc(2, 5)
+
+
+
+
+
+
+// task 7
+// function check(num1, num2) {
+//     if (num1 > num2) {
+//         return num1
+//     }
+//     return num2;
+// }
+// console.log(check(-10, -5));
+
+
+
+// task 9
+function check(num1) {
+    if (num1.toString().length == 2) {
+        for (let i = 0; i <= num1; i++) {
+            if (num1 % i === 0) {
+                console.log(i)
+            }
+        }
+    }
+    else {
+        console.log('ERROR! Check ur number!')
+    }
+}
+
+check(24)
+
+
+
+
+
+
+
+
+
+
