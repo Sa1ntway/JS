6de4e5e8ae8f3aa2f86bab4c3ecf9ea67790cb04
Sa1ntway <fryyystandoff@gmail.com>
2023-03-18 13:32:19 +0300

    18.03.23

--- JS FUNCTIONS/lesson2/task1/script.js
+++ JS FUNCTIONS/lesson2/task1/script.js
@@ -301,28 +301,75 @@
 
 
 // task5
-function mult(array) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % 2 === 0) {
-            document.write('<ins>' + array[i] + '</ins>')
+// function mult(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 === 0) {
+//             document.write('<ins>' + array[i] + '</ins>')
+//         }
+//         else {
+//             document.write(array[i])
+//         }
+//     }
+// }
+// mult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+
+
+
+
+
+
+
+// Homework
+// task6
+// function converter(money, currentCurrenсy, targetCurrenсy) {
+//     switch (currentCurrenсy) {
+//         case 'ruble':
+//             switch (targetCurrenсy) {
+//                 case 'dollar':
+//                     return money / 2.86;
+//                 case 'euro':
+//                     return money / 3.28;
+//                 case 'uah':
+//                     return money / 37;
+//             }
+//             break;
+
+//     }
+// }
+// console.log(converter(30, 'ruble', 'dollar'));
+// console.log(converter(30, 'ruble', 'euro'));
+// console.log(converter(30, 'ruble', 'uah'));
+
+
+// task7
+function translit(word) {
+    let result = ' ';
+    let converter = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+        'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
+        'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+        'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+        'ш': 'sh', 'щ': 'sch', 'ь': '', 'ы': 'y', 'ъ': '',
+        'э': 'e', 'ю': 'yu', 'я': 'ya',
+
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D',
+        'Е': 'E', 'Ё': 'E', 'Ж': 'Zh', 'З': 'Z', 'И': 'I',
+        'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
+        'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T',
+        'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Ch',
+        'Ш': 'Sh', 'Щ': 'Sch', 'Ь': '', 'Ы': 'Y', 'Ъ': '',
+        'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
+    };
+    for (let i = 0; i < word.length; i++) {
+        if (converter[word[i]] === undefined) {
+            result += word[i];
         }
         else {
-            document.write(array[i])
+            result += converter[word[i]];
         }
     }
+    return result
 }
-mult([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(translit('Сегодня был борщ на обед. Было очень вкусно!'))
---
+++ JS FUNCTIONS/lesson3/script.js
@@ -0,0 +1,83 @@
+// let arr = [-1, -2, -3, -8];
+// let result = arr.some(function (elem) {
+//     return elem > 0;
+// });
+// console.log(result)
+
+
+
+// let arr = [2, 5, 6, 10];
+// let result = arr.some(function (elem, i) {
+//     return elem * i > 30
+// });
+// console.log(result);
+
+
+// let arr = [2, 5, 6, 10];
+// let result = arr.every(function (elem) {
+//     return elem > 0
+// });
+// console.log(result)
+
+
+// let arr = [8, 10, 16, 28, 13];
+// let result = arr.every(function (elem) {
+//     return elem > 8;
+// });
+// console.log(result)
+
+
+// let arr = ['abc', 'def', 'ghi'];
+// let reverse = arr.map(function (elem, i) {
+//     return elem.split('').reverse().join("")
+// });
+// console.log(reverse)
+
+
+
+// let arr = [25, 81, 121, 4069];
+// let result = arr.map(function (elem) {
+//     return Math.sqrt(elem);
+// })
+// console.log(result)
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.map(function (elem, i) {
+//     return elem * i;
+// });
+// console.log(result)
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.forEach(function (elem) {
+//     console.log('! ' + elem);
+// })
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.forEach(function (elem) {
+//     console.log(elem + elem);
+// })
+
+
+// let arr = [-1, 4, 5, -8, 3];
+// let result = arr.filter(function (elem) {
+//     return elem > 0
+// })
+// console.log(result);
+
+
+// let arr = ['Привет', 'Лампа', 'Солнце', 'Чай', 'Душ'];
+// let result = arr.filter(function (elem) {
+//     return elem.length > 5;
+// })
+// console.log(result);
+
+
+// let arr = [2, 5, 6, 10, 11, 30];
+// let result = arr.filter(function (elem, i) {
+//     return elem * i > 30
+// })
+// console.log(result)
