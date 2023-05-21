22149eefadf559bc740377e970dc2024a5bba10e
Saintway <fryyystandoff@gmail.com>
2023-05-21 12:03:11 +0300

    21.05.23

--- DOM/lesson9/pr1/script.js
+++ DOM/lesson9/pr1/script.js
@@ -1,10 +1,99 @@
+// let startBtn = document.getElementById('start');
+// let stopBtn = document.getElementById('stop')
+// let timerId;
+// startBtn.addEventListener('click', function () {
+//     let i = 0;
+//     timerId = setInterval(function () {
+//         console.log(++i);
+//     }, 1000);
+// })
+
+// stopBtn.addEventListener('click', function () {
+//     clearInterval(timerId)
+// })
+
+
+
+// 1
+// let startBtn = document.getElementById('start');
+// let stopBtn = document.getElementById('stop')
+// let timerId;
+// startBtn.addEventListener('click', function () {
+//     let i = 0;
+//     timerId = setInterval(function () {
+//         console.log(++i);
+//     }, 3000);
+// })
+
+// stopBtn.addEventListener('click', function () {
+//     clearInterval(timerId)
+// })
+
+
+// 2
+// let startBtn = document.getElementById('start');
+// let stopBtn = document.getElementById('stop')
+// let timerId;
+// startBtn.addEventListener('click', function () {
+//     let i = 5;
+//     timerId = setInterval(function () {
+//         console.log(--i);
+//         if (i == 0) {
+//             clearInterval(timerId);
+//         }
+//     }, 1000);
+// })
+
+// stopBtn.addEventListener('click', function () {
+//     clearInterval(timerId)
+// })
+
+
+
+// 3
+// let startBtn = document.getElementById('start');
+// let i = 0;
+// startBtn.addEventListener('click', timer)
+// function timer(a) {
+//     setTimeout(function () {
+//         console.log(++i)
+
+//         timer(i);
+//     }, a * 1000)
+// }
+// timer(1)
+
+
+
+// 4
 let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop')
+let item = document.getElementById('input');
+let timerId;
 startBtn.addEventListener('click', function () {
-    let i = 0;
-    setInterval(function () {
-        document.write(++i);
+    let i = 10;
+    let itemValue = item.value;
+    timerId = setInterval(function () {
+        console.log(--itemValue)
+        item.value = itemValue
+        if (itemValue == 0) {
+            clearInterval(timerId);
+        }
+
     }, 1000);
+
 })
+
+stopBtn.addEventListener('click', function () {
+    clearInterval(timerId)
+})
+
+
+
+
+
+
+
 
 
 
---
+++ DOM/lesson9/pr2/script.js
@@ -0,0 +1,3 @@
+let arr = ['Сегодня вас ждет успех при изучении JS', 'Сегодня лучше полежите весь день на диване', 'У вас сегдня будет замечательный день', 'Сегодня лучше не выходите из дома'];
+let btn = document.getElementById('start');
+let random = document.getElementById('timer')
