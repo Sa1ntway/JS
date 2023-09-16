9cbb01729ce8d75d542bec56e2da5fe3f19919c2
Saintway <fryyystandoff@gmail.com>
2023-09-16 13:31:13 +0300

    123123

--- DOM/lesson17(Memory game project)/!Online sushi store/js/cart.js
+++ DOM/lesson17(Memory game project)/!Online sushi store/js/cart.js
@@ -1,5 +1,28 @@
 const cartWrapper = document.querySelector(".cart-wrapper");
 window.addEventListener("click", function (event) {
+  function toggleCartStatus() {
+    const cartEmpty = document.querySelector("[data-cart-empty]");
+    const cartTotal = document.querySelector(".cart-total");
+    const orderForm = document.querySelector("#order-form");
+    if (cartWrapper.querySelectorAll(".cart-item").length > 0) {
+      cartEmpty.classList.add("none");
+      cartTotal.classList.remove("none");
+      orderForm.classList.remove("none");
+    } else {
+      cartEmpty.classList.remove("none");
+      cartTotal.classList.add("none");
+      orderForm.classList.add("none");
+    }
+    let totalPrice = 0;
+    cartWrapper.querySelectorAll(".cart-item").forEach(function (item) {
+      const counter = item.querySelector("[data-counter]").innerText;
+      const priceOneItem = item.querySelector(".price__currency").innerText;
+      const price = parseInt(counter) * parseInt(priceOneItem);
+      totalPrice = totalPrice + price;
+      cartTotal.querySelector(".total-price").innerText = totalPrice;
+    });
+  }
+
   if (event.target.hasAttribute("data-cart")) {
     const cart = event.target.closest(".card");
 
@@ -8,9 +31,47 @@ window.addEventListener("click", function (event) {
       imgSrc: cart.querySelector(".product-img").getAttribute("src"),
       title: cart.querySelector(".item-title").innerText,
       itemsInBox: cart.querySelector("[data-items-in-box]").innerText,
-      weight: cart.querySelector(".price_weight").innerText,
-      price: cart.querySelector(".price_currency").innerText,
+      weight: cart.querySelector(".price__weight").innerText,
+      price: cart.querySelector(".price__currency").innerText,
       counter: cart.querySelector("[data-counter]").innerText,
     };
+
+    const itemInCart = cartWrapper.querySelector(
+      `[data-id="${productInfo.id}"]`
+    );
+
+    if (itemInCart) {
+      const counterElement = itemInCart.querySelector("[data-counter]");
+      counterElement.innerText =
+        parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+    } else {
+      const cartItemHtml = `<div class="cart-item" data-id="${productInfo.id}">  
+                        <div class="cart-item__top">  
+                            <div class="cart-item__img">  
+                                <img src="${productInfo.imgSrc}" alt="">  
+                            </div>  
+                            <div class="cart-item__desc">  
+                                <div class="cart-item__title">${productInfo.title}</div>  
+                                <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>  
+                                <!-- cart-item__details -->  
+                                <div class="cart-item__details">  
+                                    <div class="items items--small counter-wrapper">  
+                                        <div class="items__control" data-action="minus">-</div>  
+                                        <div class="items__current" data-counter="">${productInfo.counter}</div>  
+                                        <div class="items__control" data-action="plus">+</div>  
+                                    </div>  
+                                    <div class="price">  
+                                        <div class="price__currency">${productInfo.price}</div>  
+                                    </div>  
+                                </div>  
+                                <!-- // cart-item__details -->  
+                            </div>  
+                        </div>  
+                    </div>`;
+      cartWrapper.insertAdjacentHTML("beforeend", cartItemHtml);
+    }
+
+    cart.querySelector("[data-counter]").innerText = "1";
+    toggleCartStatus();
   }
 });
